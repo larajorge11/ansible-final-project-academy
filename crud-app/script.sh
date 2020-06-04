bc220f185201998d9639680743a625db8a28cfcf
Jorge Lara <larajorge11@gmail.com>
2020-06-04 18:22:05 -0500

    Jorge Lara - create playbook - connecting to azure

--- crud-app/script.sh
+++ crud-app/script.sh
@@ -6,13 +6,13 @@ sudo apt autoremove -y
 sudo systemctl enable apache2
 cd /tmp
 sudo unzip -o crud.zip
-sudo apt install mariadb-server mariadb-client -y
-sudo apt autoremove -y
-sudo systemctl enable mariadb
-sudo bash sql/setmariadb.sh
+#sudo apt install mariadb-server mariadb-client -y
+#sudo apt autoremove -y
+#sudo systemctl enable mariadb
+#sudo bash sql/setmariadb.sh
 sudo mysql -e "CREATE DATABASE crud /*\!40100 DEFAULT CHARACTER SET utf8 */;"
-sudo mysql -e "CREATE USER databaseuser@localhost IDENTIFIED BY 'abcd1234';"
-sudo mysql -e "GRANT ALL PRIVILEGES ON crud.* TO 'databaseuser'@'localhost';"
+sudo mysql -e "CREATE USER 'databaseuser'@'%'' IDENTIFIED BY 'abcd1234';"
+sudo mysql -e "GRANT ALL PRIVILEGES ON crud.* TO 'databaseuser'@'%';"
 sudo mysql -e "FLUSH PRIVILEGES;"
 sudo mysql crud < sql/users.sql
 sudo apt install php7.0 libapache2-mod-php7.0 php7.0-mysql php-common php7.0-cli php7.0-common php7.0-json php7.0-opcache php7.0-readline
