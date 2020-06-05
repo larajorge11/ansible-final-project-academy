5f438b1aebddafc957ca910fdb3b6da6abeb3d2c
Jorge Lara <larajorge11@gmail.com>
2020-06-04 22:54:34 -0500

    Jorge Lara - create playbook - connecting to azure

--- crud-app/script.sh
+++ crud-app/script.sh
@@ -11,8 +11,9 @@ sudo unzip -o crud.zip
 #sudo systemctl enable mariadb
 #sudo bash sql/setmariadb.sh
 sudo mysql -e "CREATE DATABASE crud /*\!40100 DEFAULT CHARACTER SET utf8 */;"
-sudo mysql -e "CREATE USER 'databaseuser'@'%'' IDENTIFIED BY 'abcd1234';"
-sudo mysql -e "GRANT ALL PRIVILEGES ON crud.* TO 'databaseuser'@'%';"
+#sudo mysql -e "CREATE USER 'databaseuser'@'%'' IDENTIFIED BY 'abcd1234';"
+#sudo mysql -e "GRANT ALL PRIVILEGES ON crud.* TO 'databaseuser'@'%';"
+sudo mysql -e "GRANT ALL PRIVILEGES ON *.* TO root@10.0.1.4 IDENTIFIED BY 'JORlaresp1970!' WITH GRANT OPTION;"
 sudo mysql -e "FLUSH PRIVILEGES;"
 sudo mysql crud < sql/users.sql
 sudo apt install php7.0 libapache2-mod-php7.0 php7.0-mysql php-common php7.0-cli php7.0-common php7.0-json php7.0-opcache php7.0-readline
@@ -23,4 +24,5 @@ echo "<?php phpinfo(); ?>" > /var/www/html/info.php
 sudo cp info.php /var/www/html/info.php
 sudo cp code/* /var/www/html/
 sudo chown www-data:www-data /var/www/html/ -R
+sudo apt install php libapache2-mod-php php-mysql -y
 sudo systemctl restart apache2
